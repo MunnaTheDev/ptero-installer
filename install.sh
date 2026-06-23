928baeb9e48e29c1707403191cdf956508a82a3d
MunnaTheDev <gamingwithmunna01@gmail.com>
2026-06-23 23:13:52 +0600

    install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
+set -e
 
-echo "================================="
-echo "      MunnaTheDev Installer"
-echo "================================="
+clear
+echo "======================================"
+echo "   MunnaTheDev Pterodactyl Installer"
+echo "======================================"
 
-apt update -y
-apt install -y curl wget git
+echo "[1/6] Updating system..."
+apt update -y && apt upgrade -y
 
-echo "Installation Complete!"
+echo "[2/6] Installing dependencies..."
+apt install -y curl wget git unzip tar software-properties-common
+
+echo "[3/6] Installing Docker..."
+apt install -y docker.io
+systemctl enable docker
+systemctl start docker
+
+echo "[4/6] Installing MariaDB..."
+apt install -y mariadb-server
+
+echo "[5/6] Installing Nginx..."
+apt install -y nginx
+
+echo "[6/6] Basic setup done!"
+echo "Now you need to install Panel manually or extend script."
