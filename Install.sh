5832885bb6f392c90b43e3b9305c86e76ad1a8ab
MunnaTheDev <gamingwithmunna01@gmail.com>
2026-06-23 23:09:38 +0600

    Install.sh

---
+++ Install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "================================="
+echo "      MunnaTheDev Installer"
+echo "================================="
+
+apt update -y
+apt install -y curl wget git
+
+echo "Installation Complete!"
