27a3d2ec353e1a20c2b5b8e110a632d78e24932a
MunnaTheDev <gamingwithmunna01@gmail.com>
2026-06-24 06:43:47 +0600

    install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo "🔥 Starting My Installer..."
 
 apt update -y
 apt install -y curl git
-bash <(curl -s https://ptero.nobitahost.in)
+bash <(curl -s https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer/main/install.sh)
 
 echo "✅ Installation complete!"
